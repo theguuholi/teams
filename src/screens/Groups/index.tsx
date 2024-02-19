3588aaeba020f17c5b6f2356ce987d406745226b
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-02-19 10:31:45 -0300

    styled components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Container, Title } from "./styles";
 
 export default function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>helo!!</Text>
-    </View>
+    <Container>
+      <Title>helo!!</Title>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
