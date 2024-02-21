5b1bbede4df05e0d31f09c8f5cff1fc786592ab7
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-02-21 15:27:12 -0300

    touchable opacity component

--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -3,12 +3,12 @@ import styled from "styled-components/native";
 export const Container = styled.View`
   width: 100%;
 
-  margin: 32px 0 ;
+  margin: 32px 0;
 `;
 
 export const Title = styled.Text`
   text-align: center;
-  font-size: ${({ theme }) => theme.FONT_SIZE.LG};
+  font-size: ${({ theme }) => theme.FONT_SIZE.LG}px;
   font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
   color: ${({ theme }) => theme.COLORS.WHITE};
 `;
