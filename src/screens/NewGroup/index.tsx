58bcbfae0b4b33d45c4c17676a519553813e1403
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-02-24 06:42:38 -0300

    input

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,19 @@
+import { Header } from "@components/Header";
+import { Content, Container, Icon } from "./styles";
+import { Highlight } from "@components/Highlight";
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+
+export default function NewGroup() {
+  return (
+    <Container>
+      <Header showBackButton />
+      <Content>
+        <Icon />
+        <Highlight title="Nova Turma" subtitle="Crie uma nova turma" />
+        <Input />
+        <Button title="Criar" style={{ marginTop: 20 }} />
+      </Content>
+    </Container>
+  );
+}
