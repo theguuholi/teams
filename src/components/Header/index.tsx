9db197a8082aff56a6ecf3f0d247a55f49ad1897
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-02-27 06:41:23 -0300

    Add navigation functionality to Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
+import { useNavigation } from "@react-navigation/native";
 import { BackButton, BackIcon, Container, Logo } from "./styles";
 import logoImg from "@assets/logo.png";
 type Props = {
   showBackButton?: boolean;
 };
 export function Header({ showBackButton = false }: Props) {
+  const navigation = useNavigation();
+  function handleGoBack() {
+    navigation.navigate("groups");
+  }
+
   return (
     <Container>
       {showBackButton && (
-        <BackButton>
+        <BackButton onPress={handleGoBack}>
           <BackIcon />
         </BackButton>
       )}
