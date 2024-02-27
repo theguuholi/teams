daf59d2074656c4d40588c8d1848d1e961dbe67d
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-02-27 06:15:01 -0300

    configuracao de rotas

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import Groups from "@screens/Groups";
 import { ThemeProvider } from "styled-components/native";
 import theme from "./src/theme";
 import {
@@ -8,8 +7,7 @@ import {
 } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/Loading";
 import { StatusBar } from "react-native";
-import NewGroup from "@screens/NewGroup";
-import Players from "@screens/Players";
+import { Routes } from "@routes/index";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -21,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
