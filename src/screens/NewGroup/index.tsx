18b601d9e348a6d23380d7927a1196ad469767b4
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-02-29 15:45:57 -0300

    cadastrar grupo

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -6,16 +6,27 @@ import { Input } from "@components/Input";
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
+import { Alert } from "react-native";
 
 export default function NewGroup() {
   const [group, setGroup] = useState("");
   const navigation = useNavigation();
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", "Digite o nome do grupo");
+      }
+
       await groupCreate(group);
       navigation.navigate("players", { group });
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Nao foi possivel criar um novo grupo");
+        console.log(error);
+      }
     }
   }
 
