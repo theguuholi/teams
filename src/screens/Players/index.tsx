831baff369192af934b965512715cd0f1cf6f5d7
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-03-04 09:56:24 -0300

    inputref

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -4,14 +4,13 @@ import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
-import { Alert, FlatList } from "react-native";
-import { useState } from "react";
+import { Alert, FlatList, TextInput } from "react-native";
+import { useEffect, useRef, useState } from "react";
 import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 import { useRoute } from "@react-navigation/native";
 import { AppError } from "@utils/AppError";
-import { playersGetByGroup } from "@storage/player/playersGetByGroup";
 import { playerAddByGroup } from "@storage/player/playerAddByGroup";
 import { playersGetByGroupAndTeam } from "@storage/player/playerGetByGroupAndTeam";
 import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO";
@@ -26,6 +25,7 @@ export default function Players() {
   const [players, setPlayers] = useState<PlayerStorageDTO[]>([]);
   const route = useRoute();
   const { group } = route.params as RouteParams;
+  const newPlayerNameInputRef = useRef<TextInput>(null);
 
   async function handleAddPlayer() {
     if (newPlayerName.trim().length === 0) {
@@ -35,8 +35,11 @@ export default function Players() {
 
     try {
       await playerAddByGroup(newPlayer, group);
-      const players = await playersGetByGroup(group);
-      console.log(players);
+      // const players = await playersGetByGroup(group);
+      // console.log(players);
+      newPlayerNameInputRef.current?.blur();
+      setNewPlayerName("");
+      fetchPlayersByTeam();
     } catch (error) {
       if (error instanceof AppError) {
         Alert.alert("new player", error.message);
@@ -56,6 +59,14 @@ export default function Players() {
     }
   }
 
+  // arrow function, array de dependencia, no array quando mudar ele vai executar o use effect novamente
+  // o use effect nao aceita async await
+  useEffect(() => {
+    console.log("useEffect executou");
+    console.log(team);
+    fetchPlayersByTeam();
+  }, [team]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -66,9 +77,13 @@ export default function Players() {
 
       <Form>
         <Input
+          inputRef={newPlayerNameInputRef}
           placeholder="Nome da pessoa"
           onChangeText={setNewPlayerName}
+          value={newPlayerName}
           autoCorrect={false}
+          onSubmitEditing={handleAddPlayer}
+          returnKeyType="done"
         />
         <ButtonIcon icon="add" onPress={handleAddPlayer} />
       </Form>
@@ -92,9 +107,9 @@ export default function Players() {
 
       <FlatList
         data={players}
-        keyExtractor={(item) => item}
+        keyExtractor={(item) => item.name}
         renderItem={({ item }) => (
-          <PlayerCard name={item} onRemove={() => {}} />
+          <PlayerCard name={item.name} onRemove={() => {}} />
         )}
         ListEmptyComponent={() => (
           <ListEmpty message="Nao ha pessoas nesse time" />
