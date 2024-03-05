ec3324a0bf47cea992b133de2d15447047208053
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-03-05 15:34:02 -0300

    Add player removal functionality to Players screen

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -14,6 +14,7 @@ import { AppError } from "@utils/AppError";
 import { playerAddByGroup } from "@storage/player/playerAddByGroup";
 import { playersGetByGroupAndTeam } from "@storage/player/playerGetByGroupAndTeam";
 import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO";
+import { playerRemoveByGroup } from "@storage/player/playerRemoveByGroup";
 
 type RouteParams = {
   group: string;
@@ -56,6 +57,17 @@ export default function Players() {
     } catch (error) {
       console.log(error);
       Alert.alert("Error fetching players");
+    }
+  }
+
+  async function handlePlayerRemove(playerName: string) {
+    try {
+      console.log("removendo", playerName, group);
+      await playerRemoveByGroup(playerName, group);
+      fetchPlayersByTeam();
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Error removing player");
     }
   }
 
@@ -109,7 +121,10 @@ export default function Players() {
         data={players}
         keyExtractor={(item) => item.name}
         renderItem={({ item }) => (
-          <PlayerCard name={item.name} onRemove={() => {}} />
+          <PlayerCard
+            name={item.name}
+            onRemove={() => handlePlayerRemove(item.name)}
+          />
         )}
         ListEmptyComponent={() => (
           <ListEmpty message="Nao ha pessoas nesse time" />
