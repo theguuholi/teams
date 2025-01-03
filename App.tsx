8768a21fb5e5d86c1b9168c92750f3a309591d65
Gus <g.92oliveira@gmail.com>
2025-01-03 10:42:02 -0300

    start

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Groups from './src/screens/Groups';
+import Groups from '@screens/Groups';
+import { View } from 'react-native';
 
 export default function App() {
   return (
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Groups() {
     return (
         <View style={styles.container}>
-            
+            <Text>Hello!</Text>
         </View>
     );
 }
