20a669fec1216114d25b77fb89a4835cfa6aa194
Gus <g.92oliveira@gmail.com>
2025-01-03 11:23:21 -0300

    componente loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import { Container, LoadIndicator } from "./styles";
+
+const Loading = () => {
+    return (
+        <Container>
+            <LoadIndicator />
+        </Container>
+    );
+}
+export default Loading;
