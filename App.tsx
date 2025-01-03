65d054f086ef4400826d71c60966c30f4aa73e68
Gus <g.92oliveira@gmail.com>
2025-01-03 11:08:33 -0300

    Utilizando o thema

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import Groups from '@screens/Groups';
+import { ThemeProvider } from 'styled-components';
+import theme from '@theme/index';
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Groups />
       <StatusBar style="auto" />
-    </>
+    </ThemeProvider>
   );
 }
