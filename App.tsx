3886d7e1578eecdf0dc1bfb1286a8b1325f89fec
Gus <g.92oliveira@gmail.com>
2025-01-03 10:54:41 -0300

    utilizando styled components

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import Groups from '@screens/Groups';
-import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
+    <>
       <Groups />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 https://github.com/tleunen/babel-plugin-module-resolver/blob/master/DOCS.md
+https://styled-components.com/
+
+<!-- >  npx expo install react-native-web@~0.18.7 react-dom@18.0.0 @expo/webpack-config@^0.17.0 -->
+> npx expo install react-dom
+❯ npm install styled-components
