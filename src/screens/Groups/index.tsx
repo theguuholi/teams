66ac95147ceb51afe5c971d0c5153fd2b2dd3445
Gus <g.92oliveira@gmail.com>
2025-01-04 07:11:51 -0300

    listando grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,14 +2,22 @@ import Header from '@components/Header';
 import { Container } from './styles';
 import Highlight from '@components/Higlight';
 import GroupCard from '@components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 export default function Groups() {
+    const [groups, setGroups] = useState<string[]>(['elixir']);
+
     return (
         <Container>
             <Header />
             <Highlight title="Teams" subtitle="Play with your Friends" />
 
-            <GroupCard title="Group 1" />
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => <GroupCard title={item} />}
+            />
         </Container>
     );
 }
